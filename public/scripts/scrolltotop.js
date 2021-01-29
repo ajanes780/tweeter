8d1752cda97220f1719890adb5886dc9844b0ec6
Aaron Janes <Aaron.Janes@gmail.com>
2021-01-28 20:27:12 -0700

    added scroll to top function

--- public/scripts/scrolltotop.js
+++ public/scripts/scrolltotop.js
@@ -1,9 +1,8 @@
 $(document).ready(function () {
   //Get the button:
-  mybutton = document.getElementById("myBtn");
-
-  // top = document.getElementById("top");
-  // When the user scrolls down 20px from the top of the document, show the button
+  let mybutton = document.getElementById("myBtn");
+  let rootElement = document.documentElement;
+  mybutton.addEventListener("click", scrollToTop);
   window.onscroll = function () {
     scrollFunction();
   };
@@ -19,8 +18,10 @@ $(document).ready(function () {
     }
   }
 
-  function topFunction() {
-    document.documentElement.scrollTop = 1;
+  function scrollToTop() {
+    rootElement.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
   }
-  // When the user clicks on the button, scroll to the top of the document
 });
